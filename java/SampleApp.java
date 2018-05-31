f0f46ebf0f17335abb58e37e3a339f7a088985d7
manuba11 <manabu.izurumachi@gmail>
2018-05-31 13:21:21 +0900

    My first commit

---
+++ java/SampleApp.java
@@ -0,0 +1,25 @@
+//Commnet
+
+public class SampleApp 
+{
+    public static void main(String[] args)
+    {
+        Test i = new Test(); //generate instans
+        i.printHello("Hello"); //call 
+    }
+}
+
+class Test
+{
+    private String x; //can't get form outside
+
+    public Test()
+    {
+        this.x = "";
+    }
+
+    public void printHello(String mes) //method. It's can call from outside
+    {
+        System.out.println(mes);
+    }
+}
