5b1c94e89e1ca881a505efcefbed7020566213ff
manuba11 <manabu.izurumachi@gmail.com>
2018-07-05 11:31:56 +0900

    Studying how to get Java class name.

--- java/Learn0003.java
+++ java/Learn0003.java
@@ -1,38 +1,48 @@
+//import java.lang.Class;
 import java.util.Arrays;
 
 class Learn0003
 {
     public static void main(String[] args)
     {
-        Integer[] iVar = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        short[] shVar = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        //Initialize  array with initial value.
+        byte[] byVar = {117, 118, 119, 120,  121, 123, 124, 125, 126, 127};
+//        short[] shVar = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] inVar = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1000000000};
+//        long[] loVar = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        System.out.println(iVar.length);
-
-        for (int i = 0; i < iVar.length; i++)
-        {
-            System.out.println(iVar[i]);
-        }
-
-        //Initialize array with same value.
-        //Need import Arrays
-        Arrays.fill(iVar, 30006);
-
-        for (int i = 0; i < iVar.length; i++)
-        {
-            System.out.println(iVar[i]);
-        }
+//        System.out.println(inVar.length);
+//
+//        for (int i = 0; i < inVar.length; i++)
+//        {
+//            System.out.println(inVar[i]);
+//        }
+//
+//        //Initialize array with same value.
+//        //Need import Arrays library
+//        Arrays.fill(inVar, 30000);
+//
+//        for (int i = 0; i < inVar.length; i++)
+//        {
+//            System.out.println(inVar[i]);
+//        }
+//
 
         //Cast variables of different types to each other
-        for (int i = 0; i < iVar.length; i++)
+        for (int i = 0; i < inVar.length; i++)
         {
-            iVar[i] = (int)shVar[i];
+            //Cast short to Integer with '(int)'
+            inVar[i] = (byte)byVar[i];
         }
 
-        for (int i = 0; i < iVar.length; i++)
+        for (int i = 0; i < inVar.length; i++)
         {
-            System.out.println(iVar[i]);
+            System.out.println(inVar[i]);
         }
+        //Get class name from instance variable.
+        System.out.println(byVar.getClass().getName());
+        //Get arrys length with Arrays.length().
+        System.out.println(byVar.length);
     }
 }
 
